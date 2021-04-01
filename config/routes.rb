fa63d37b7f32ac8dbc8f559f6bfa36e590942797
Jeremy Grift <jgrift@academic.rrc.ca>
2021-03-31 19:24:25 -0500

    Add Category model/view/controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :products, only: %i[index show]
+  resources :categories, only: %i[index]
 
   # get 'products/index'
   # get 'products/show'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,13 @@
+Product.delete_all
+Category.delete_all
+
 Product.create(name: "CPU", description: "Good CPU", price: 11_111)
 Product.create(name: "Graphics Card", description: "Good Graphics Card", price: 111_111)
 
+Category.create(name: "CPU's", description: "All the best CPU's you could dream of!")
+
 puts "Created #{Product.all.count} Products."
+puts "Created #{Category.all.count} Categories."
+# puts "Created #{Product.all.count} Products."
+# puts "Created #{Product.all.count} Products."
+# puts "Created #{Product.all.count} Products."
