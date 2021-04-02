f3bc21718a38e4d7b2194fe7b0a9d92972da7eba
Jeremy Grift <jgrift@academic.rrc.ca>
2021-04-02 15:56:22 -0500

    Add basic product image storage with ActiveStorage

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,9 @@ class Product < ApplicationRecord
   has_many :order_products
   has_many :orders, through: :order_products
   accepts_nested_attributes_for :orders, allow_destroy: true
+  has_one_attached :image_small
+  has_one_attached :image_med
+  has_one_attached :image_large
 
   validates :name, :price, presence: true
   validates :price, numericality: { only_integer: true }
