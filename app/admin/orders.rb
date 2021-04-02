b72485e9e5d3d19ce1b07be1ff3def4fbb83d9fd
Jeremy Grift <jgrift@academic.rrc.ca>
2021-04-02 12:38:39 -0500

    set to default, will come back later

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,19 +1,22 @@
 ActiveAdmin.register Order do
-  permit_params :customer, :total, :status, :PST, :GST, :HST, order_products: %i[price quantity],
-                                                              products:       %i[name description category_id]
+  permit_params :customer, :total, :status, :PST, :GST, :HST,
+                order_products: %i[price quantity product: [name description category_id]]
 
-  form do |f|
-    f.semantic_errors # shows errors on :base
-    # f.inputs          # builds an input field for every attribute
-    f.inputs
-    f.has_many :products do |n_f|
-      n_f.input :name
-    end
-    # f.inputs do
-    #   f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image) : ""
-    # end
-    f.actions # adds the 'Submit' and 'Cancel' buttons
-  end
+  # form do |f|
+  #   f.semantic_errors # shows errors on :base
+  #   # f.inputs          # builds an input field for every attribute
+  #   f.inputs
+  #   # f.has_many :order_products do |o_f|
+  #   #   # # o_f.has_one :products do |p_f|
+  #   #   # f.input products: :name
+  #   #   # # end
+  #   #   o_f.input :price
+  #   #   o_f.input :quantity
+  #   # end
+  #   # f.inputs do
+  #   #   f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image) : ""
+  #   # end
+  #   f.actions # adds the 'Submit' and 'Cancel' buttons
 
   #   form do |f|
   #     f.semantic_errors # shows errors on :base
@@ -29,5 +32,6 @@ ActiveAdmin.register Order do
   #         n_f.input :name
   #       end
   #     end
+  #   end
   # end
 end
