7212fc146bf8f808bac911e9ed8ff4e70f466bb8
Jeremy Grift <jgrift@academic.rrc.ca>
2021-04-02 11:16:27 -0500

    displays proudct name, but no quantity/price

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -6,12 +6,8 @@ ActiveAdmin.register Order do
     f.semantic_errors # shows errors on :base
     # f.inputs          # builds an input field for every attribute
     f.inputs
-    f.has_many :order_products do |o_f|
-      # # o_f.has_one :products do |p_f|
-      # f.input products: :name
-      # # end
-      o_f.input :price
-      o_f.input :quantity
+    f.has_many :products do |n_f|
+      n_f.input :name
     end
     # f.inputs do
     #   f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image) : ""
@@ -19,18 +15,19 @@ ActiveAdmin.register Order do
     f.actions # adds the 'Submit' and 'Cancel' buttons
   end
 
-  # form do |f|
-  #   f.semantic_errors # shows errors on :base
-  #   # f.inputs          # builds an input field for every attribute
-  #   f.inputs do
-  #     f.input :customer_id
-  #     f.input :total
-  #     f.input :status
-  #     f.input :PST
-  #     f.input :GST
-  #     f.input :HST
-  #     f.has_many :products do |n_f|
-  #       n_f.input :name
+  #   form do |f|
+  #     f.semantic_errors # shows errors on :base
+  #     # f.inputs          # builds an input field for every attribute
+  #     f.inputs do
+  #       f.input :customer_id
+  #       f.input :total
+  #       f.input :status
+  #       f.input :PST
+  #       f.input :GST
+  #       f.input :HST
+  #       f.has_many :products do |n_f|
+  #         n_f.input :name
+  #       end
   #     end
-  #   end
+  # end
 end
