7b81932af56d930691b2eae80e0194546be8ea91
Jeremy Grift <jgrift@academic.rrc.ca>
2021-04-13 16:59:20 -0500

    Update uer/customer reference to be optional

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  belongs_to :customer
+  belongs_to :customer, optional: true
 end
