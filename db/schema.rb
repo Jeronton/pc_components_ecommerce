1bb9893eda6df52299c6c6e6881d54b8f18c6aa3
Jeremy Grift <jgrift@academic.rrc.ca>
2021-04-14 20:23:54 -0500

    Add stripe_session to orders table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_14_010356) do
+ActiveRecord::Schema.define(version: 2021_04_15_012219) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -110,6 +110,7 @@ ActiveRecord::Schema.define(version: 2021_04_14_010356) do
     t.integer "HST"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "stripe_session"
     t.index ["customer_id"], name: "index_orders_on_customer_id"
   end
 
