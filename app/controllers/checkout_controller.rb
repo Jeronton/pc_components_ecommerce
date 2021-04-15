425ea39680a52a71f1662def12af8a9c7396a796
Jeremy Grift <jgrift@academic.rrc.ca>
2021-04-14 20:56:53 -0500

    Move apply_shipping logic to orders#create

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -62,38 +62,4 @@ class CheckoutController < ApplicationController
   def shipping
     @customer = user_signed_in? ? current_user.customer : Customer
   end
-
-  def apply_shipping
-    # puts inputs.inspect
-    inputs = params[:customer]
-    province = if inputs[:province_id].nil?
-                 nil
-               else
-                 Province.find(inputs[:province_id])
-               end
-    cust = Customer.find_or_create_by(
-      first_name:    inputs[:first_name],
-      middle_name:   inputs[:middle_name],
-      last_name:     inputs[:last_name],
-      address_line1: inputs[:address_line1],
-      address_line2: inputs[:address_line2],
-      country:       inputs[:country],
-      province:      province,
-      city:          inputs[:city],
-      postal:        inputs[:postal],
-      email:         inputs[:email],
-      phone:         inputs[:phone]
-    )
-
-    if cust.valid?
-      # set the current customer to this customr
-      session[:customer_id] = cust.id
-
-      redirect_to checkout_payment_path
-
-    else
-      puts cust.errors.inspect
-      redirect_to request.referer
-    end
-  end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,65 +15,103 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @items = cart_items
+    # get and save shipping details
+    inputs = params[:customer]
+    province = if inputs[:province_id].nil?
+                 nil
+               else
+                 Province.find(inputs[:province_id])
+               end
+    cust = Customer.find_or_create_by(
+      first_name:    inputs[:first_name],
+      middle_name:   inputs[:middle_name],
+      last_name:     inputs[:last_name],
+      address_line1: inputs[:address_line1],
+      address_line2: inputs[:address_line2],
+      country:       inputs[:country],
+      province:      province,
+      city:          inputs[:city],
+      postal:        inputs[:postal],
+      email:         inputs[:email],
+      phone:         inputs[:phone]
+    )
 
-    @customer = Customer.find(session[:customer_id]) unless session[:customer_id].nil?
+    if cust.valid?
+      # set the current customer to this customr
+      session[:customer_id] = cust.id
 
-    if @items.nil? || @items.size == 0
-      flash[:notice] = "❌ Must have at least one item to prepare order."
-      redirect_to request.referer
-    elsif @customer.nil?
-      flash[:notice] = "❌ Error fetching shipping details for order."
-      redirect_to request.referer
-    end
+      # valid customer data, so create order summary
+      @items = cart_items
+      @customer = Customer.find(cust.id) unless session[:customer_id].nil?
 
-    # where the total/taxes will be stored
-    subtotal = 0
-    pst = 0
-    gst = 0
-    hst = 0
-
-    if @customer.valid?
-      order = Order.create(
-        customer: @customer,
-        total:    0,
-        status:   "incomplete",
-        PST:      pst,
-        GST:      gst,
-        HST:      hst
-      )
-
-      # add products to the order and calculate taxes
-      @items.each do |item|
-        product = item[:product]
-        quantity = item[:quantity]
-        OrderProduct.create(
-          order:    order,
-          product:  product,
-          price:    product.price,
-          quantity: quantity
-        )
-
-        # Add to total and taxes.
-        subtotal += product.price * quantity
-        pst += product.price * quantity * @customer.province.PST unless @customer.province.PST.nil?
-        gst += product.price * quantity * @customer.province.GST unless @customer.province.GST.nil?
-        hst += product.price * quantity * @customer.province.HST unless @customer.province.HST.nil?
+      if @items.nil? || @items.size == 0
+        flash[:notice] = "❌ Must have at least one item to prepare order."
+        redirect_to request.referer
+        return
+      elsif @customer.nil?
+        flash[:notice] = "❌ Error fetching shipping details for order."
+        redirect_to request.referer
+        return
       end
 
-      # calculate the full total
-      total = subtotal + pst + gst + hst
+      # where the total/taxes will be stored
+      subtotal = 0
+      pst = 0
+      gst = 0
+      hst = 0
 
-      # Update the order with the calculated totals
-      order.update(
-        total:  total,
-        status: "unsubmitted",
-        PST:    pst,
-        GST:    gst,
-        HST:    hst
-      )
+      if @customer.valid?
+        order = Order.create(
+          customer: @customer,
+          total:    0,
+          status:   "incomplete",
+          PST:      pst,
+          GST:      gst,
+          HST:      hst
+        )
 
-      redirect_to(order) if order.valid?
+        # add products to the order and calculate taxes
+        @items.each do |item|
+          product = item[:product]
+          quantity = item[:quantity]
+          OrderProduct.create(
+            order:    order,
+            product:  product,
+            price:    product.price,
+            quantity: quantity
+          )
+
+          # Add to total and taxes.
+          subtotal += product.price * quantity
+          unless @customer.province.PST.nil?
+            pst += product.price * quantity * @customer.province.PST
+          end
+          unless @customer.province.GST.nil?
+            gst += product.price * quantity * @customer.province.GST
+          end
+          unless @customer.province.HST.nil?
+            hst += product.price * quantity * @customer.province.HST
+          end
+        end
+
+        # calculate the full total
+        total = subtotal + pst + gst + hst
+
+        # Update the order with the calculated totals
+        order.update(
+          total:  total,
+          status: "unsubmitted",
+          PST:    pst,
+          GST:    gst,
+          HST:    hst
+        )
+
+        redirect_to(order) if order.valid?
+      end
+    else
+      # ivalid shipping details, will have to find way to display errors.
+      puts cust.errors.inspect
+      redirect_to request.referer
     end
   end
 end
