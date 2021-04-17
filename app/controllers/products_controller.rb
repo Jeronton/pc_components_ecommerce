20a8a2d55e2671a9d5d8fee60b7a05bca4ff7d64
Jeremy Grift <jgrift@academic.rrc.ca>
2021-04-17 14:47:35 -0500

    Add product category dropdown to header
    And added logic to products index to filter by category if it is provided

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,12 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.includes(:category).all
+    category_id = begin
+      Integer(params[:category])
+    rescue StandardError
+      -1
+    end
+    @products = @products.where(category_id: params[:category]) unless category_id < 0
   end
 
   def show
