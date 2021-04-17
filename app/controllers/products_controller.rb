b10a11359f775b29ca8809489f3df019dfce7763
Jeremy Grift <jgrift@academic.rrc.ca>
2021-04-17 16:00:54 -0500

    Add search functionality to products index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,8 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.includes(:category).all
+
+    # if category is passed then filter by it.
     category_id = begin
       Integer(params[:category])
     rescue StandardError
@@ -8,6 +10,16 @@ class ProductsController < ApplicationController
     end
     @category = Category.find(Integer(params[:category])) unless category_id < 0
     @products = @products.where(category_id: params[:category]) unless category_id < 0
+
+    # if search paramerter exists, search by it
+    search_string = "%#{params['search'].downcase}%"
+    unless params["search"].nil? || params["search"] == ""
+      @products = @products.where(
+        "LOWER(name) LIKE :search OR LOWER(description) LIKE :search",
+        search: search_string
+      )
+    end
+    @products
   end
 
   def show
